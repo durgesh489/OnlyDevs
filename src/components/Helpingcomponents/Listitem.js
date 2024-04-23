5d9776e519901daab82ffba604b8f40e92ffd5e6
Aditya sharma <adityasharma5847@gmail.com>
2024-04-23 20:07:08 +0530

    seprate Signup page for user and expert added with proper rounting new Experts detail page added with dynamic data home button added modal added with create portal

--- src/components/Helpingcomponents/Listitem.js
+++ src/components/Helpingcomponents/Listitem.js
@@ -30,26 +30,26 @@ const Listitem = (props) => {
     //   </div>
     // </React.Fragment>
 
-    <div className="max-w-sm mx-auto bg-sky-400 text-white m-6 shadow-md rounded-lg overflow-hidden">
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{props.Title}</div>
-        <p className=" text-base">
-          <span className="font-semibold">Name:</span> {props.FirstName}{" "}
-          {props.LastName}
-        </p>
-        <p className=" text-base">
-          <span className="font-semibold">Company:</span> {props.company}
-        </p>
-        <p className=" text-base">
-          <span className="font-semibold">Experience:</span> {props.experience}
-        </p>
-        <p className=" text-base">
-          <span className="font-semibold">Description:</span>{" "}
-          {props.Description}
-        </p>
-      </div>
-      </div>
-      
+    // <div className="max-w-sm mx-auto bg-sky-400 text-white m-6 shadow-md rounded-lg overflow-hidden">
+    //   <div className="px-6 py-4">
+    //     <div className="font-bold text-xl mb-2">{props.Title}</div>
+    //     <p className=" text-base">
+    //       <span className="font-semibold">Name:</span> {props.FirstName}{" "}
+    //       {props.LastName}
+    //     </p>
+    //     <p className=" text-base">
+    //       <span className="font-semibold">Company:</span> {props.company}
+    //     </p>
+    //     <p className=" text-base">
+    //       <span className="font-semibold">Experience:</span> {props.experience}
+    //     </p>
+    //     <p className=" text-base">
+    //       <span className="font-semibold">Description:</span>{" "}
+    //       {props.Description}
+    //     </p>
+    //   </div>
+    //   </div>
+
     // <ListItem>
     //   <ListItemAvatar>
     //     <Avatar>
@@ -67,6 +67,45 @@ const Listitem = (props) => {
     //     }
     //   />
     // </ListItem>
+
+    <React.Fragment>
+      <div className="flex shadow-md bg-white w-[60%] m-auto rounded-lg ">
+        <div className="w-[35%]">Image</div>
+        <div className="flex flex-col  border-2 w-full pt-3 px-3">
+          <div className=" pl-2  w-full">
+            <div className="flex gap-4 items-center ">
+              <p className="text-2xl font-bold ">{props.Name}</p>
+              <p className="text-lg font-Times font-medium">{props.company}</p>
+            </div>
+            <div className="font-normal">
+              {props.techStack.join(" || ")}
+              {/* HTML || CSS || JavaScript || React.js || MySql */}
+            </div>
+            <p className="font-medium">
+              {props.experience}+ Years of Experience
+            </p>
+            <div className="text-lg">
+              {props.Description}
+              {/* "Amidst the lush greenery of the garden, a vibrant array of
+              flowers bloomed, each petal a burst of color under the golden
+              sunlight. Bees buzzed lazily, collecting nectar, while butterflies
+              danced in the gentle breeze, creating a picturesque scene of
+              nature's beauty and tranquility." */}
+            </div>
+          </div>
+          <div className=" p-2 flex justify-evenly items-center">
+            <button className=" p-2 w-[6rem] rounded-lg text-white font-bold bg-sky-600">
+              {" "}
+              Contact
+            </button>
+            <button className=" p-2 w-[6rem] rounded-lg text-white font-bold bg-sky-600">
+              {" "}
+              Info
+            </button>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
---
+++ src/components/Modal/UserdefineModal.js
@@ -0,0 +1,53 @@
+import CloseIcon from "@mui/icons-material/Close";
+import { BrowserRouter, Link } from "react-router-dom";
+import React from "react";
+
+const UserdefinModal = (props) => {
+  const closeButtonClickHandler = () => {
+    props.visibilityHandler();
+  };
+
+  return (
+    <React.Fragment>
+      <div
+        className={`${
+          props.visibility ? " " : "hidden"
+        }  z-40 fixed shadow-black translate-x-[142%] translate-y-[-100%] flex flex-col justify-center rounded-3xl text-2xl items-center w-[400px] h-[400px] bg-slate-400  `}
+      >
+        <div className="absolute right-4 top-3 ">
+          <button
+            onClick={closeButtonClickHandler}
+            className="bg-white rounded-lg"
+          >
+            <CloseIcon color="black" fontSize="large" />
+          </button>
+        </div>
+        <div className="flex flex-col w-full justify-center items-center font-bold">
+          <p>Want to Sign Up as :</p>
+          <div>
+            <Link to="/User">
+              <button
+                onClick={closeButtonClickHandler}
+                className="bg-white p-2 m-3 w-[7rem] rounded-full"
+              >
+                User
+              </button>
+            </Link>
+          </div>
+          <div>
+            <Link to="/Expert">
+              <button
+                onClick={closeButtonClickHandler}
+                className="bg-white p-2 m-3 w-[7rem] rounded-full"
+              >
+                Expert
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default UserdefinModal;
